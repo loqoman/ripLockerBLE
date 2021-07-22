362d66569f542fd1f1c4a93b630b5a8535695aaa
loqoman <darwin.s.clark@gmail.com>
2021-07-22 15:06:09 -0700

    :bulb: Header onto peripheralMain

--- src/peripheralMain.cpp
+++ src/peripheralMain.cpp
@@ -1,3 +1,11 @@
+// Personal/Programming/Projects/ripeLockerBLE
+// Darwin Clark
+//   for RipeLocker LLC
+
+// GOAL: Have this file be less than 100 lines
+
+// [X] Scan for the other BLE feather and see it 
+// [X] Send a custom message between the feathers
 
 // NB. THIS IS CONNECTABLE FROM THE PHONE AND NEEDS TO BE DEBUGABBLE FROM THE PHONE
 
