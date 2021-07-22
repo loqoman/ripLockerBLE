ef7c83603f0b854b884f428aaa34c74f57750e18
loqoman <darwin.s.clark@gmail.com>
2021-07-22 15:04:34 -0700

    :sparkles: Finished modularizing the client-side code.

--- lib/client/clientLib.h
+++ lib/client/clientLib.h
@@ -1,18 +1,20 @@
 #include <bluefruit.h>
 #include "messageLib.h"
-BLEClientUart clientUART;
 
+// TODO: This seems like a c++-specific problem that can be solved in a much more gracious way
+BLEClientUart clientUart;
 
 // ===== Callbacks ===== // 
 // I *could* fit the callbacks inside the client object
-void scanCallback(ble_gap_evt_adv_report_t* report);    // When we scan and find something
+void scanCallback(ble_gap_evt_adv_report_t* report);            // When we scan and find something
 
-void connectCallback(uint16_t conn_handle);     // When we make a new connection
+void connectCallback(uint16_t conn_handle);                     // When we make a new connection
 
 void disconnectCallback(uint16_t conn_handle, uint8_t reason);  // When we disconnect
 
-void UARTrxCallback(BLEClientUart& uart_svc);   // When we get a message
+void UARTrxCallback(BLEClientUart& uart_svc);                   // When we get a message
 
 
 // ===== Helper Functiosn ======
-void setupScanner(); // 
+void setupScanner(); 
+
