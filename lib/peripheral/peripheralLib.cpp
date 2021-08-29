419049d067aa09d5bc065aa075d4524e5a77b169
loqoman <darwin.s.clark@gmail.com>
2021-08-29 12:33:28 -0700

    :sparkles: Massive work on `peripheral`. Adding a place to write the handling of the message which will sync up with the MAX485 modules

--- lib/peripheral/peripheralLib.cpp
+++ lib/peripheral/peripheralLib.cpp
@@ -6,8 +6,10 @@
 #include "messageLib.h"
 
 extern BLEUart bleuart;
+extern bool awake;
+extern double timeToSleep; 
 
-// ===== Callbacks ===== // 
+// ===== BLUETOOTH Callbacks ===== // 
 void periphConnectCallback(uint16_t conn_handle) {
 // Get the reference to current connection
     BLEConnection* connection = Bluefruit.Connection(conn_handle);
@@ -29,15 +31,26 @@ void periphDisconnectCallback(uint16_t conn_handle, uint8_t reason) {
 
     // When we(peripheral) get a message
 void UARTrxCallback(BLEClientUart& uart_svc) {
-    Serial.print("[Periph][RX]: ");
-
-    while (uart_svc.available() ) {
-        Serial.print((char) uart_svc.read() );
+    // Serial.print("[Client][Debug]: Available: ");
+    // Serial.println(String(uart_svc.available()));
+    
+	Serial.print("[Peripheral][RX]: ");
+    // TODO: Revisit this number as messages grow in size. This is very very bad because this will lead us to only partially read messages
+    // IDEA: add brackets to either end of the message to veryify the start and end, then implement code that waits until the next read
+	if (uart_svc.available() < 65) {
+        return;
     }
+    String readMessage;
+	while (uart_svc.available()) {
+        readMessage = ((char) uart_svc.read()); 
+		Serial.print(readMessage);
+	}
 
-
-    Serial.println();
+	Serial.println();    
 }
+
+// ===== MESSAGE Callbacks ====== //
+
 
 // ====== Helper Function ====== // 
 void setupAdvertising() {
@@ -80,9 +93,40 @@ void setupAdvertising() {
     * https://developer.apple.com/library/content/qa/qa1931/_index.html
     */
     Bluefruit.Advertising.restartOnDisconnect(true);
-    Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
-    Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
-    Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
+    Bluefruit.Advertising.setInterval(32, 244);         // in unit of 0.625 ms
+    Bluefruit.Advertising.setFastTimeout(30);           // number of seconds in fast mode
+    Bluefruit.Advertising.start(0);                     // 0 = Don't stop advertising after n seconds
 }
 
+void handleMessage(String message) {
+    // Figur eout what message it is
+    Message inputMessage = Message::decodeFromWire(message);
 
+    switch (inputMessage.type) {
+        case Message::messageType::REQBATT:
+            REQRBATTCallback();
+        case Message::messageType::KEEPALIVE:
+            KEEPALIVECallback();
+            break;
+        case Message::messageType::REQREAD:
+            REQREADCallback();
+            break;
+        case Message::messageType::REQRUONDTRIP:
+            REQROUNDTRIPCallback();
+            break;
+        case Message::messageType::GOTOSLEEP:
+            GOTOSLEEPCallback();
+            break;
+        case Message::messageType::SETSLEEPTIME:
+            SETSLEEPTIMECallback();
+            break;
+
+            
+    }
+
+}
+
+void KEEPALIVECallback() {
+
+    
+}
--- lib/peripheral/peripheralLib.h
+++ lib/peripheral/peripheralLib.h
@@ -1,12 +1,24 @@
 #include <bluefruit.h>
-
-// ===== Callbacks ===== // 
+#include "messageLib.h"
+// ===== BLUETOOTH Callbacks ===== // 
 void periphConnectCallback(uint16_t conn_handle);
-
 void periphDisconnectCallback(uint16_t conn_handle, uint8_t reason);
-
 void UARTrxCallback(BLEClientUart& uart_svc);   // When we(peripheral) get a message
+
+// ===== MESSAGE Callbacks ===== //
+
+// Likely will need access to global scope variables
+// TODO: This feels like a place for metaprogramming, but I don't know enough C++ to properly implemnt
+//        a template or something similar
+void KEEPALIVECallback();
+void REQRBATTCallback();
+void REQREADCallback();
+void REQROUNDTRIPCallback();
+void GOTOSLEEPCallback();
+void SETSLEEPTIMECallback();
+
 
 // ===== Helper Functions ===== //
 
 void setupAdvertising();
+void handleMessage(String message);
